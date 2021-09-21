631b577f2d1d92866b556eb36ed78de24e2b40c9
Filipe_Natividade <n_filipe_n@hotmail.com>
2021-09-21 19:12:51 -0300

    yarn add highlight

--- src/Components/content/index.tsx
+++ src/Components/content/index.tsx
@@ -1,5 +1,7 @@
 import { useState, ChangeEvent } from 'react'
 import marked from 'marked'
+import highlight from 'highlight.js'
+import 'highlight.js/styles/vs.css'
 import { File } from '@styled-icons/boxicons-regular/File'
 
 import {
@@ -10,6 +12,8 @@ import {
   TexteareaContainer,
   MarkedownContainer,
 } from './style'
+
+highlight.highlightAll()
 
 const Content = () => {
   const [content, setContent] = useState('')
