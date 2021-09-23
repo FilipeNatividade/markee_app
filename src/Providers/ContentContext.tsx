884cf35ed683406c62f52e82ba39b63dfad0ab83
Filipe_Natividade <n_filipe_n@hotmail.com>
2021-09-23 00:26:03 -0300

    functions update & selectItem & deleteFile/done

--- src/Providers/ContentContext.tsx
+++ src/Providers/ContentContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, RefObject, useRef, ReactNode, SetStateAction, Dispatch } from 'react'
+import { createContext, useContext, useState, RefObject, useRef, ReactNode, SetStateAction, Dispatch, useEffect } from 'react'
 import { v4 as idv4 } from 'uuid'
 
 type TypeFile = {
@@ -9,13 +9,20 @@ type TypeFile = {
   status: string
 }
 
+
 type TypeCreate = {
   files: TypeFile[]
   setFiles: Dispatch<SetStateAction<TypeFile[]>>
+  titleFile: string
+  setTitleFile: Dispatch<SetStateAction<string>>
+  contentFile: string
+  setContentFile: Dispatch<SetStateAction<string>>
   createNewFile: any
   inputRef: RefObject<HTMLInputElement>
+  selectFile: any
+  deleteFile: any
 }
-
+// arrumar esses dois type any
 
 type TypeChildren = {
   children: ReactNode | ReactNode[]
@@ -25,6 +32,49 @@ const ContentContext = createContext<TypeCreate>({} as TypeCreate)
 
 export const GlobalProvider = ({ children }: TypeChildren) => {
   const [files, setFiles] = useState<TypeFile[]>([])
+  const [titleFile, setTitleFile] = useState<string>('')
+  const [contentFile, setContentFile] = useState<string>('')
+
+  useEffect(() => {
+    let timer: ReturnType<typeof setTimeout>
+
+    const updateSutatus = () => {
+      const file = files.find(item => item.active === true)
+
+      if (!file || file.status !== 'editing') {
+        return
+      }
+
+      timer = setTimeout(() => {
+        setFiles(files => files.map(item => {
+          if (file.active) {
+            return {
+              ...item,
+              status: 'saving',
+            }
+          }
+          return file
+        }))
+
+        setTimeout(() => {
+          setFiles(files => files.map(item => {
+            if (item.active) {
+              return {
+                ...item,
+                status: 'saved',
+              }
+            }
+
+            return file
+          }))
+        }, 300)
+      }, 300)
+    }
+
+    updateSutatus()
+    return () => clearTimeout(timer)
+  }, [files])
+
   const inputRef = useRef<HTMLInputElement>(null)
 
   const createNewFile = () => {
@@ -42,8 +92,36 @@ export const GlobalProvider = ({ children }: TypeChildren) => {
         status: 'saved',
       }))
   }
+
+  const selectFile = (id: string) => (event: MouseEvent) => {
+    event.preventDefault()
+
+    inputRef.current?.focus()
+
+    setFiles(files => files.map(item => ({
+      ...item,
+      active: item.id === id,
+    })))
+  }
+
+  const deleteFile = (id: string) => {
+    setFiles(files => files.filter(item => item.id !== id))
+  }
   return (
-    <ContentContext.Provider value={{ files, setFiles, createNewFile, inputRef }}>
+    <ContentContext.Provider
+      value={{
+        files,
+        setFiles,
+        titleFile,
+        setTitleFile,
+        contentFile,
+        setContentFile,
+        createNewFile,
+        inputRef,
+        selectFile,
+        deleteFile,
+      }}
+    >
       {children}
     </ContentContext.Provider>
   )
